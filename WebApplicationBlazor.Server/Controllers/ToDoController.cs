12b23fdd55bc6460eb8b7f13f3b44f0c10ce2d69
Nemanja Ljubinkovic <nemanja.ljubinkovic@comdata.rs>
2019-02-21 13:18:40 +0100

    ToDo initial

---
+++ WebApplicationBlazor.Server/Controllers/ToDoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplicationBlazor.Server.Model;
+
+namespace WebApplicationBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        private List<ToDoItem> toDoItems = new List<ToDoItem>
+        {
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<ToDoItem> Items()
+        {
+            return toDoItems;
+        }
+
+        [HttpPost("[action]")]
+        public void Items(ToDoItem toDoItem)
+        {
+            this.toDoItems.Add(toDoItem);
+        }
+
+        [HttpDelete("[action]")]
+        public void Items(int id)
+        {
+            toDoItems.Remove(toDoItems.First(x => x.Id == id));
+        }
+    }
+}
