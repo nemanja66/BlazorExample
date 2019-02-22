3b19d502dda7bb051a027421d57aa4dcf790e092
Dusko Beronja <dusko.beronja@comdata.rs>
2019-02-22 09:42:31 +0100

    Container

--- WebApplicationBlazor.Shared/ToDoItem.cs
+++ WebApplicationBlazor.Shared/ToDoItem.cs
@@ -1,9 +1,23 @@
-﻿namespace WebApplicationBlazor.Shared
+﻿using System;
+
+namespace WebApplicationBlazor.Shared
 {
     public class ToDoItem
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public DateTime DueDate { get; private set; }
+
+        private int hours;
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                hours = value;
+                DueDate = DateTime.Now.AddHours(hours);
+            }
+        }
     }
 }
