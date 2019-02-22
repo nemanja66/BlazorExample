425211abb366566e1b65183ce690d88a8c2192b7
Dusko Beronja <dusko.beronja@comdata.rs>
2019-02-22 07:16:05 +0100

    Added jsInterop

---
+++ WebApplicationBlazor.Client/wwwroot/js/app.js
@@ -0,0 +1,14 @@
+﻿window.toDoInterop = {
+    confirmDelete: function (title) {
+        $('#toDoTitleField').text(title);
+        $('#myModal').modal('show');
+
+        return true;
+    },
+
+    hideDeleteDialog: function () {
+        $('#myModal').modal('hide');
+
+        return true;
+    },
+}
